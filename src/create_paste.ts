e9758342e009fd76b98280160bd150818966a33b
Le-Val <le-val1@protonmail.ch>
2021-12-30 07:09:58 -0500

    feat: docstrigs

--- src/create_paste.ts
+++ src/create_paste.ts
@@ -1,3 +1,7 @@
+/*
+ * creates a paste and returns the Url
+ * NOTE: use parsePokemonToString() function to pass a pokemon object to the function
+ */
 export async function createPaste(author: string, paste: string, notes: string, title: string) {
   const { url } = await fetch(`https://pokepast.es/create?author=${author}&paste=${paste}&notes=${notes}&title=${title}`, {
     method: "POST"
--- src/get_paste.ts
+++ src/get_paste.ts
@@ -1,12 +1,14 @@
 import type { Paste, RawPaste } from "./types/paste.ts";
 import { parseStringToPokemon } from "./parser.ts";
 
+/* gets the raw content from a paste */
 export async function getRawPaste(id: string): Promise<RawPaste> {
   const paste = await fetch(`https://pokepast.es/${id}/json`);
 
   return paste.json();
 }
 
+/* gets a paste */
 export async function getPaste(id: string): Promise<Paste> {
   const { author, notes, paste, title } = await getRawPaste(id);
 
