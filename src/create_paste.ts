b4da1b796666c2e3a6cf45c57c64a498c7ffaf03
Le-Val <le-val1@protonmail.ch>
2021-12-30 09:46:14 -0500

    make some parameters optional

--- src/create_paste.ts
+++ src/create_paste.ts
@@ -2,9 +2,17 @@
  * creates a paste and returns the Url
  * NOTE: use parsePokemonToString() function to pass a pokemon object to the function
  */
-export async function createPaste(author: string, paste: string, notes: string, title: string) {
-  const { url } = await fetch(`https://pokepast.es/create?author=${author}&paste=${paste}&notes=${notes}&title=${title}`, {
-    method: "POST"
-  });
+export async function createPaste(
+  paste: string,
+  author?: string,
+  notes?: string,
+  title?: string
+) {
+  const { url } = await fetch(
+    `https://pokepast.es/create?paste=${paste}${author ? `&author=${author}` : ""}${notes ? `&notes=${notes}` : ""}${title ? `&title=${title}` : ""}`,
+    {
+      method: "POST",
+    }
+  );
   return url;
 }
